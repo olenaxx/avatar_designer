005fc28b65931185fe314670def5ad49da87c3ea
Olena Stadnyk <96227346+olenaxx@users.noreply.github.com>
2023-10-31 20:28:31 +0200

    feat: Implemented the border color

--- avatar-designer/main.js
+++ avatar-designer/main.js
@@ -1,9 +1,18 @@
 const inputRange = document.querySelector('.input-range')
+const inputColor = document.querySelector('.input-color')
 const avatarImage = document.querySelector('.avatar')
+
+avatarImage.style.border = "4px solid #FF851B"
+avatarImage.style.borderRadius = "50px"
+
 
 inputRange.addEventListener("change", (e) => {
   const rangeValue = e.currentTarget.value
   avatarImage.style.borderRadius = Number.parseInt(rangeValue, 10) + 'px'
 })
 
-avatarImage.style.border = "4px solid #FF851B"
+inputColor.addEventListener("change", (e) => {
+  const colorValue = e.currentTarget.value
+  avatarImage.style.border = "4px solid" + colorValue
+})
+
