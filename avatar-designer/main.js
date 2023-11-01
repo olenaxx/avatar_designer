9f7a6072d50702e71ff5fe07f8bed77cbf58af13
Olena Stadnyk <96227346+olenaxx@users.noreply.github.com>
2023-11-01 10:34:39 +0200

    feat: Implemented the image buttons

--- avatar-designer/main.js
+++ avatar-designer/main.js
@@ -1,15 +1,32 @@
 const inputRange = document.querySelector('.input-range')
 const inputColor = document.querySelector('.input-color')
 const avatarImage = document.querySelector('.avatar')
+const avatarImages = document.querySelectorAll('.avatar')
+const buttons = document.querySelectorAll('.button')
 
-
+// Add an event listener to the border radius slider and set the value
 inputRange.addEventListener("change", (e) => {
   const rangeValue = e.currentTarget.value
   avatarImage.style.borderRadius = Number.parseInt(rangeValue, 10) + 'px'
 })
 
+// Add an event listener to the color picker and set the border color
 inputColor.addEventListener("change", (e) => {
   const colorValue = e.currentTarget.value
   avatarImage.style.border = "4px solid" + colorValue
 })
 
+// Add a click event listener to each button
+buttons.forEach(btn => {
+
+  btn.addEventListener('click', () => {
+    // Get the image path from the data-image attribute using dataset
+    const imagePath = btn.dataset.image;
+
+    // Loop through avatar images and set the image source
+    avatarImages.forEach(avatar => {
+      avatar.src = imagePath;
+    })
+  })
+
+})
