c9e583127ce5167a49ef6e59eaa2de797370400f
Olena Stadnyk <96227346+olenaxx@users.noreply.github.com>
2023-11-02 15:29:41 +0200

    feat: selected border-radius and color values in localStorage

--- avatar-designer/main.js
+++ avatar-designer/main.js
@@ -9,14 +9,17 @@ const inputRangeSize = document.querySelector('.input-range-size')
 
 // Add an event listener to the border radius slider and set the value
 inputRange.addEventListener("change", (e) => {
+  e.preventDefault()
   const rangeValue = e.currentTarget.value
   avatarImage.style.borderRadius = Number.parseInt(rangeValue, 10) + 'px'
+  localStorage.setItem('borderRadius', rangeValue)
 })
 
 // Add an event listener to the color picker and set the border color
 inputColor.addEventListener("change", (e) => {
   const colorValue = e.currentTarget.value
   avatarImage.style.border = "4px solid" + colorValue
+  localStorage.setItem('color', colorValue)
 })
 
 // Add a click event listener to each button
@@ -38,7 +41,6 @@ buttons.forEach(btn => {
 // Implement the file upload
 inputFile.addEventListener('change', (e) => {
   const selectedFile = e.currentTarget.files[0]
-  console.log(e.currentTarget.files[0]);
 
   if (selectedFile) {
 
@@ -54,9 +56,27 @@ inputFile.addEventListener('change', (e) => {
   }
 })
 
+
 // additional slider to control the image size
 inputRangeSize.addEventListener('change', (e) => {
   const imageSize = e.currentTarget.value;
+  console.log(imageSize);
   avatarImage.style.width = imageSize + "px"
   avatarImage.style.height = imageSize + "px"
 })
+
+
+// Load the border radius from localStorage and set it
+const savedBorderRadius = localStorage.getItem('borderRadius') || "50"
+if (savedBorderRadius) {
+  avatarImage.style.borderRadius = savedBorderRadius + 'px'
+}
+inputRange.value = savedBorderRadius
+
+
+// Load the border color from localStorage and set it
+const savedBorderColor = localStorage.getItem('color') || `#FF851B`
+if (savedBorderColor) {
+  avatarImage.style.border = "4px solid" + savedBorderColor
+}
+inputColor.value = savedBorderColor
